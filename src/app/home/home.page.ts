0f843b883e9885058eed90d53fb177185b4029cc
resonar09 <darrenperez@gmail.com>
2018-12-04 18:24:26 -0500

    Fixed Profit Loss, Added Shares

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,8 +13,8 @@ export class HomePage {
   private fundsAvaliable: Number;
   private fundsAvaliablePerTrade: Number;
   private stockEntry: Number;
-  private stockExit: Number;
-  private stockStop: Number;
+  private stockExit: String;
+  private stockStop: String;
   private stockLoss: String;
   private stockProfit: String;
   private percentageGainLossMin: Number = -4;
@@ -49,20 +49,23 @@ export class HomePage {
     this.onStockChange();
   }
   onStockChange() {
-    console.log('onStockChange ' + this.stockEntry + ':' + this.percentageLoss.valueOf() / 100);
     if (this.stockEntry > 0) {
-        this.stockStop = this.stockEntry.valueOf() + (this.stockEntry.valueOf() * this.percentageLoss.valueOf() / 100);
-        this.stockExit = this.stockEntry.valueOf() + (this.stockEntry.valueOf() * this.percentageGain.valueOf() / 100);
+        this.stockStop = this.setDecimal(this.stockEntry.valueOf() + (this.stockEntry.valueOf() * this.percentageLoss.valueOf() / 100));
+        this.stockExit = this.setDecimal(this.stockEntry.valueOf() + (this.stockEntry.valueOf() * this.percentageGain.valueOf() / 100));
         if (this.fundsAvaliablePerTrade > 0) {
+          console.log('onStockChange ' + this.stockEntry + ':' + this.percentageLoss.valueOf() / 100);
+          this.sharesAvailable = Math.floor(this.fundsAvaliablePerTrade.valueOf() / this.stockEntry.valueOf());
           // tslint:disable-next-line:max-line-length
-          this.stockLoss = this.getCurrency((this.stockEntry.valueOf() * this.percentageLoss.valueOf() / 100) * this.fundsAvaliablePerTrade.valueOf());
+          this.stockLoss = this.getCurrency((this.stockEntry.valueOf() * this.percentageLoss.valueOf() / 100) * this.sharesAvailable.valueOf());
           // tslint:disable-next-line:max-line-length
-          this.stockProfit = this.getCurrency((this.stockEntry.valueOf() * this.percentageGain.valueOf() / 100) * this.fundsAvaliablePerTrade.valueOf());
-          this.sharesAvailable = this.fundsAvaliablePerTrade.valueOf() / this.stockEntry.valueOf();
+          this.stockProfit = this.getCurrency((this.stockEntry.valueOf() * this.percentageGain.valueOf() / 100) * this.sharesAvailable.valueOf());
         }
     }
   }
   getCurrency(amount: Number): String {
     return amount.toString();
+  }
+  setDecimal(number: Number): String {
+    return number.toFixed(4);
   }
 }
